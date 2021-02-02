6e3a8e703d92949259e367bfffebfd64ce2c7870
Gustavo Tiecker <gustavotiecker@gmail.com>
2021-02-02 13:33:34 -0300

    test: ensure that RemoteAddAccount completes with error when HttpClient fails

---
+++ Data/Http/HttpError.swift
@@ -0,0 +1,13 @@
+//
+//  HttpError.swift
+//  Data
+//
+//  Created by Gustavo Tiecker on 02/02/21.
+//  Copyright © 2021 Gustavo Tiecker. All rights reserved.
+//
+
+import Foundation
+
+public enum HttpError: Error {
+    case noConnection
+}
---
+++ Domain/Errors/DomainError.swift
@@ -0,0 +1,13 @@
+//
+//  DomainError.swift
+//  Domain
+//
+//  Created by Gustavo Tiecker on 02/02/21.
+//  Copyright © 2021 Gustavo Tiecker. All rights reserved.
+//
+
+import Foundation
+
+public enum DomainError: Error {
+    case unexpected
+}
