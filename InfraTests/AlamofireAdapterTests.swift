104fc86a3e0f6c71cc9d4c03c26b73ff28e585a9
Gustavo Tiecker <gustavotiecker@gmail.com>
2021-02-08 22:05:57 -0300

    feat: added infra layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,16 @@
+//
+//  AlamofireAdapterTests.swift
+//  AlamofireAdapterTests
+//
+//  Created by Gustavo Tiecker on 08/02/21.
+//  Copyright © 2021 Gustavo Tiecker. All rights reserved.
+//
+
+import XCTest
+@testable import Infra
+
+class AlamofireAdapterTests: XCTestCase {
+    
+    func test_() {
+    }
+}
