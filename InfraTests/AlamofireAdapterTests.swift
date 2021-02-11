adae70098b7679b0b819f486c8de10e811adee87
Gustavo Tiecker <gustavotiecker@gmail.com>
2021-02-11 18:20:40 -0300

    refactor: expectation code moved to post completion handler

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -76,14 +76,12 @@ extension AlamofireAdapterTests {
     
     func testRequestFor(url: URL = makeURL(), data: Data?, action: @escaping (URLRequest) -> Void) {
         let sut = makeSut()
-        sut.post(to: url, with: data) { _ in }
-        
         let exp = expectation(description: "waiting")
-        UrlProtocolStub.observedRequest { request in
-            action(request)
-            exp.fulfill()
-        }
+        sut.post(to: url, with: data) { _ in exp.fulfill() }
+        var request: URLRequest?
+        UrlProtocolStub.observedRequest { request = $0 }
         wait(for: [exp], timeout: 1)
+        action(request!)
     }
 }
 
