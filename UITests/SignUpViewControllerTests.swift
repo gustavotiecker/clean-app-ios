a421c725fb664bda1bc0dfa1eaf0726eacf5a2cb
Gustavo Tiecker <gustavotiecker@gmail.com>
2021-02-19 10:32:08 -0300

    feat: UI layer added

---
+++ UITests/SignUpViewControllerTests.swift
@@ -0,0 +1,15 @@
+//
+//  UITests.swift
+//  UITests
+//
+//  Created by Gustavo Tiecker on 19/02/21.
+//  Copyright © 2021 Gustavo Tiecker. All rights reserved.
+//
+
+import XCTest
+
+class UITests: XCTestCase {
+    
+    func test_() {
+    }
+}
