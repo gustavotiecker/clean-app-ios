65187cdca9f8c91a3493e8ae40359865823c0275
Gustavo Tiecker <gustavotiecker@gmail.com>
2021-02-24 10:33:24 -0300

    feat: adding Validation target

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,15 @@
+//
+//  EmailValidatorAdapterTests.swift
+//  EmailValidatorAdapterTests
+//
+//  Created by Gustavo Tiecker on 24/02/21.
+//  Copyright © 2021 Gustavo Tiecker. All rights reserved.
+//
+
+import XCTest
+
+class EmailValidatorAdapterTests: XCTestCase {
+    
+    func test_() {
+    }
+}
