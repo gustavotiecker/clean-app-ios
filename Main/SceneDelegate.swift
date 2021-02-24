a2a9913fcc45692ce811f9c4882a775c6727057c
Gustavo Tiecker <gustavotiecker@gmail.com>
2021-02-24 11:17:40 -0300

    feat: adding Main target

---
+++ Main/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  Main
+//
+//  Created by Gustavo Tiecker on 24/02/21.
+//  Copyright © 2021 Gustavo Tiecker. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
