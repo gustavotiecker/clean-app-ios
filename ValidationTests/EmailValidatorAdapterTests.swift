6ed6dbe8f2a03bca2789764a08ff44d9f9196690
Gustavo Tiecker <gustavotiecker@gmail.com>
2021-02-24 10:51:11 -0300

    refactor: moving EmailValidatorAdapter to its own file

--- ValidationTests/EmailValidatorAdapterTests.swift
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -8,22 +8,12 @@
 
 import XCTest
 import Presentation
-
-public class EmailValidatorAdapter: EmailValidator {
-    
-    private let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-    
-    public func isValid(email: String) -> Bool {
-        let range = NSRange(location: 0, length: email.utf16.count)
-        let regex = try! NSRegularExpression(pattern: pattern)
-        return regex.firstMatch(in: email, options: [], range: range) != nil
-    }
-}
+import Validation
 
 class EmailValidatorAdapterTests: XCTestCase {
     
     func test_invalid_emails() {
-        let sut = EmailValidatorAdapter()
+        let sut = makeSut()
         XCTAssertFalse(sut.isValid(email: "user"))
         XCTAssertFalse(sut.isValid(email: "user@"))
         XCTAssertFalse(sut.isValid(email: "user@example"))
@@ -32,8 +22,15 @@ class EmailValidatorAdapterTests: XCTestCase {
     }
     
     func test_valid_emails() {
-        let sut = EmailValidatorAdapter()
+        let sut = makeSut()
         XCTAssertTrue(sut.isValid(email: "user@example.com"))
         XCTAssertTrue(sut.isValid(email: "user@example.com.br"))
+    }
+}
+
+extension EmailValidatorAdapterTests {
+    
+    func makeSut() -> EmailValidatorAdapter {
+        return EmailValidatorAdapter()
     }
 }
