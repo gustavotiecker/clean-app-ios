397b84466074812d42c5994e1298738321a67779
Gustavo Tiecker <gustavotiecker@gmail.com>
2021-03-02 17:05:13 -0300

    refactor: moving threading logic to a Decorator class

--- MainTests/Composers/SignUpComposerTests.swift
+++ MainTests/Composers/SignUpComposerTests.swift
@@ -13,8 +13,17 @@ import UI
 class SignUpComposerTests: XCTestCase {
 
     func test_background_request_should_complete_on_main_thread() {
-        let (sut, _) = makeSut()
+        let (sut, addAccountSpy) = makeSut()
+        let exp = expectation(description: "waiting")
+        
         sut.loadViewIfNeeded()
+        sut.signUp?(makeSignUpViewModel())
+        
+        DispatchQueue.global().async {
+            addAccountSpy.completeWithError(.unexpected)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
     }
 }
 
@@ -22,7 +31,7 @@ extension SignUpComposerTests {
     
     func makeSut(file: StaticString = #filePath, line: UInt = #line) -> (sut: SignUpViewController, addAccountSpy: AddAccountSpy){
         let addAccountSpy = AddAccountSpy()
-        let sut = SignUpComposer.composeControllerWith(addAccount: addAccountSpy)
+        let sut = SignUpComposer.composeControllerWith(addAccount: MainQueueDispatchDecorator(addAccountSpy))
         
         checkMemoryLeak(for: sut, file: file, line: line)
         checkMemoryLeak(for: addAccountSpy, file: file, line: line)
--- Presentation/Presenters/SignUp/SignUpPresenter.swift
+++ Presentation/Presenters/SignUp/SignUpPresenter.swift
@@ -30,13 +30,13 @@ public final class SignUpPresenter {
             loadingView.display(viewModel: LoadingViewModel(isLoading: true))
             addAccount.add(addAccountModel: SignUpMapper.toAddAccountModel(viewModel: viewModel)) { [weak self] result in
                 guard let self = self else { return }
+                self.loadingView.display(viewModel: LoadingViewModel(isLoading: false))
                 
                 switch result {
                 case .failure(_): self.alertView.showMessage(viewModel: AlertViewModel(title: "Erro", message: "Algo inesperado aconteceu, tente novamente em alguns instantes"))
                 case .success(_):
                     self.alertView.showMessage(viewModel: AlertViewModel(title: "Sucesso", message: "Conta criada com sucesso"))
                 }
-                self.loadingView.display(viewModel: LoadingViewModel(isLoading: false))
             }
         }
     }
