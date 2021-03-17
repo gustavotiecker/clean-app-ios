6fc99ef7f1361d2060f8c9ffc2152691736f5cee
Gustavo Tiecker <gustavotiecker@gmail.com>
2021-03-16 22:51:57 -0300

    refactor: move RequiredFieldValidation to its own file

---
+++ Validation/Validators/RequiredFieldValidation.swift
@@ -0,0 +1,28 @@
+//
+//  RequiredFieldValidation.swift
+//  Validation
+//
+//  Created by Gustavo Tiecker on 16/03/21.
+//  Copyright © 2021 Gustavo Tiecker. All rights reserved.
+//
+
+import Foundation
+import Presentation
+
+public final class RequiredFieldValidation: Validation {
+    
+    private let fieldName: String
+    private let fieldLabel: String
+    
+    public init(fieldName: String, fieldLabel: String) {
+        self.fieldName = fieldName
+        self.fieldLabel = fieldLabel
+    }
+    
+    public func validate(data: [String : Any]?) -> String? {
+        guard let fieldName = data?[fieldName] as? String, !fieldName.isEmpty else {
+            return "O campo \(fieldLabel) é obrigatório"
+        }
+        return nil
+    }
+}
--- ValidationTests/RequiredFieldValidationTests.swift
+++ ValidationTests/RequiredFieldValidationTests.swift
@@ -8,48 +8,39 @@
 
 import XCTest
 import Presentation
-
-class RequiredFieldValidation: Validation {
-    
-    private let fieldName: String
-    private let fieldLabel: String
-    
-    init(fieldName: String, fieldLabel: String) {
-        self.fieldName = fieldName
-        self.fieldLabel = fieldLabel
-    }
-    
-    func validate(data: [String : Any]?) -> String? {
-        guard let fieldName = data?[fieldName] as? String, !fieldName.isEmpty else {
-            return "O campo \(fieldLabel) é obrigatório"
-        }
-        return nil
-    }
-}
+import Validation
 
 class RequiredFieldValidationTests: XCTestCase {
 
     func test_validate_should_return_error_if_field_is_not_provided() {
-        let sut = RequiredFieldValidation(fieldName: "email", fieldLabel: "Email")
+        let sut = makeSut(fieldName: "email", fieldLabel: "Email")
         let errorMessage = sut.validate(data: ["name": "Pedro"])
         XCTAssertEqual(errorMessage, "O campo Email é obrigatório")
     }
     
     func test_validate_should_return_error_with_correct_field_label() {
-        let sut = RequiredFieldValidation(fieldName: "age", fieldLabel: "Idade")
+        let sut = makeSut(fieldName: "age", fieldLabel: "Idade")
         let errorMessage = sut.validate(data: ["name": "Pedro"])
         XCTAssertEqual(errorMessage, "O campo Idade é obrigatório")
     }
     
     func test_validate_should_return_nil_if_field_is_provided() {
-        let sut = RequiredFieldValidation(fieldName: "email", fieldLabel: "Email")
+        let sut = makeSut(fieldName: "email", fieldLabel: "Email")
         let errorMessage = sut.validate(data: ["email": "pedro@teste.com"])
         XCTAssertNil(errorMessage)
     }
     
     func test_validate_should_return_nil_if_data_is_not_provided() {
-        let sut = RequiredFieldValidation(fieldName: "email", fieldLabel: "Email")
+        let sut = makeSut(fieldName: "email", fieldLabel: "Email")
         let errorMessage = sut.validate(data: nil)
         XCTAssertEqual(errorMessage, "O campo Email é obrigatório")
+    }
+}
+
+extension RequiredFieldValidationTests {
+    func makeSut(fieldName: String, fieldLabel: String, file: StaticString = #filePath, line: UInt = #line) -> Validation {
+        let sut = RequiredFieldValidation(fieldName: fieldName, fieldLabel: fieldLabel)
+        checkMemoryLeak(for: sut, file: file, line: line)
+        return sut
     }
 }
