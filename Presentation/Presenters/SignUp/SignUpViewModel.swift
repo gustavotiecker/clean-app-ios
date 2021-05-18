1052f467865a1108c67ea5373e38e4ca8f6398c2
Gustavo Tiecker <gustavotiecker@gmail.com>
2021-05-17 22:55:27 -0300

    refactor: remove SignUpMapper

--- Presentation/Presenters/SignUp/SignUpViewModel.swift
+++ Presentation/Presenters/SignUp/SignUpViewModel.swift
@@ -21,4 +21,8 @@ public struct SignUpViewModel: Model {
         self.password = password
         self.passwordConfirmation = passwordConfirmation
     }
+    
+    public func toAddAccountModel() -> AddAccountModel {
+        return AddAccountModel(name: name!, email: email!, password: password!, passwordConfirmation: passwordConfirmation!)
+    }
 }
