93d590672d831f4f6be4f9c057be9e23ea75ca18
Gustavo Tiecker <gustavotiecker@gmail.com>
2021-08-20 23:20:21 -0300

    test: ensure that RemoteAddAccount completer with emailInUseError when HttpClient completes with forbbiden

--- DataTests/UseCases/RemoteAddAccountTests.swift
+++ DataTests/UseCases/RemoteAddAccountTests.swift
@@ -34,6 +34,14 @@ class RemoteAddAccountTests: XCTestCase {
         })
     }
     
+    func test_add_should_complete_with_email_in_use_error_if_client_completes_with_forbidden() {
+        let (sut, httpClientSpy) = makeSut()
+        
+        expect(sut, completeWith: .failure(.emailInUse), when: {
+            httpClientSpy.completedWithError(.forbidden)
+        })
+    }
+    
     func test_add_should_complete_with_error_if_client_completes_with_valid_data() {
         let (sut, httpClientSpy) = makeSut()
         let account = makeAccountModel()
